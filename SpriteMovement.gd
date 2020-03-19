0ef7203e747e8f4fae720f176c88d7ffd54f502e
Ryan Anderson <rdanderson521@gmail.com>
2020-03-19 21:21:40 +0000

    testing

--- SpriteMovement.gd
+++ SpriteMovement.gd
@@ -2,14 +2,34 @@ extends Sprite
 
 var hex = load("res://HexOperations.gd").Hex
 
+var speed = 150
+var moves = Array()
+var selected = true
+var hex_pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	self.position = hex.hex_to_point(hex.hex_round_axial(Vector2(rand_range(0,5),rand_range(0,5))))
-	pass # Replace with function body.
+	hex_pos = Vector2(int(rand_range(0,5)),int(rand_range(0,5)))
+	self.position = hex.hex_to_point(hex.hex_round_axial(hex_pos))
+	pass
 
+func _input(event):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		print("sprite: "+str(event.position))
+		self.on_click(event.position)
 
+func on_click(click_position):
+	var hex_coord = hex.point_to_hex(click_position)
+	print("global")
+	print (get_global_mouse_position())
+	var hex_centre = hex.hex_to_point(hex_coord)
+	print("hex")
+	print(hex_coord)
+	print (hex_centre)
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
--- TileMapClick.gd
+++ TileMapClick.gd
@@ -6,43 +6,43 @@ extends TileMap
 
 var hex = load("res://HexOperations.gd").Hex
 
+var click_position
+var draw_list = Array()
+
 func _ready():
 	self.cell_custom_transform.y.y = sqrt(3)*(self.cell_size.x/2)
 	self.cell_custom_transform.x.y = (sqrt(3)*(self.cell_size.x/2))/2
 	self.cell_size.y = sqrt(3)*(self.cell_size.x/2)
-
-var draw_list = Array()
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
+		print("tilemap: "+str(event.position))
 		self.on_click(event.position)
 
 func on_click(click_position):
 	print("Click")
 	var camera = self.get_child(0)
 	var global_click_position =  (camera.get_camera_position() + (( click_position - camera.get_viewport().get_visible_rect().size/2) * camera.scale * camera.get_zoom()))
-	print(global_click_position)
-	print(get_global_mouse_position())
-	
+
 	var hex_coord = hex.point_to_hex(global_click_position)
 	
-	set_cellv(hex_coord,-1)
+	#set_cellv(hex_coord,-1)
 	
 	var hex_centre = hex.hex_to_point(hex_coord)
-	print (hex_centre)
-	var points = Array()
-	points.push_back(Vector2(hex_centre.x-8,hex_centre.y-14))
-	points.push_back(Vector2(hex_centre.x+8,hex_centre.y-14))
-	points.push_back(Vector2(hex_centre.x+16,hex_centre.y))
-	points.push_back(Vector2(hex_centre.x+8,hex_centre.y+14))
-	points.push_back(Vector2(hex_centre.x-8,hex_centre.y+14))
-	points.push_back(Vector2(hex_centre.x-16,hex_centre.y))
-	points.push_back(Vector2(hex_centre.x-8,hex_centre.y-14))
-	draw_list.push_back(points)
-	print (draw_list.size())
+	#print (hex_centre)
+	#var points = Array()
+	#points.push_back(Vector2(hex_centre.x-8,hex_centre.y-14))
+	#points.push_back(Vector2(hex_centre.x+8,hex_centre.y-14))
+	#points.push_back(Vector2(hex_centre.x+16,hex_centre.y))
+	#points.push_back(Vector2(hex_centre.x+8,hex_centre.y+14))
+	#points.push_back(Vector2(hex_centre.x-8,hex_centre.y+14))
+	#points.push_back(Vector2(hex_centre.x-16,hex_centre.y))
+	#points.push_back(Vector2(hex_centre.x-8,hex_centre.y-14))
+	#draw_list.push_back(points)
+	#print (draw_list.size())
 	
 
 func _draw():
